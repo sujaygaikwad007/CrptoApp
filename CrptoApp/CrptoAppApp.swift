3278f33c29c4d4c91f7950318e32ccdb92de9f73
Aniket Patil <aniket.p@reapmind.com>
2024-05-29 20:46:52 +0530

    Initial Commit

---
+++ CrptoApp/CrptoAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  CrptoAppApp.swift
+//  CrptoApp
+//
+//  Created by Aniket Patil on 29/05/24.
+//
+
+import SwiftUI
+
+@main
+struct CrptoAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
