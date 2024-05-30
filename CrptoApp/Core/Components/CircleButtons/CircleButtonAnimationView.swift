4a4d4c3720ab58d914d0189efe2a4bd9b2777477
Aniket Patil <aniket.p@reapmind.com>
2024-05-30 22:03:06 +0530

    Home View

--- CrptoApp/Core/Components/CircleButtons/CircleButtonAnimationView.swift
+++ CrptoApp/Core/Components/CircleButtons/CircleButtonAnimationView.swift
@@ -1,13 +1,23 @@
 import SwiftUI
 
 struct CircleButtonAnimationView: View {
+    
+    @Binding  var animate:Bool
+    
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        
+        Circle()
+            .stroke(lineWidth: 5.0)
+            .scale(animate ? 1.0 : 0.0)
+            .opacity(animate ? 0.0 : 1.0)
+            .animation(animate ? Animation.easeInOut(duration: 1.0) : .none)
+            
     }
 }
 
 struct CircleButtonAnimationView_Previews: PreviewProvider {
     static var previews: some View {
-        CircleButtonAnimationView()
+        CircleButtonAnimationView(animate: .constant(false))
+        
     }
 }
--- CrptoApp/Core/Home/Views/HomeView.swift
+++ CrptoApp/Core/Home/Views/HomeView.swift
@@ -10,31 +10,43 @@ struct HomeView: View {
             
             //Content
             VStack{
+                homeHeader
                 
-                HStack{
-                    CircleButtonView(iconName: showPortfolio ? "plus" : "info")
-                        .animation(.none)
-                    Spacer()
-                    Text(showPortfolio ? "Portfolio" : "Live Prices")
-                        .font(.headline)
-                        .fontWeight(.heavy)
-                        .foregroundColor(Color.theme.accent)
-                        .animation(.none)
-                    Spacer()
-                    CircleButtonView(iconName: "chevron.right")
-                        .rotationEffect(Angle(degrees: showPortfolio ? 180 : 0))
-                        .onTapGesture {
-                            withAnimation(.spring()) {
-                                self.showPortfolio.toggle()
-                            }
-                        }
-                    
-                }
-                .padding(.horizontal)
                 Spacer(minLength: 0)
             }
             
         }
+    }
+}
+
+
+
+extension HomeView{
+    
+    private var homeHeader : some View{
+        HStack{
+            CircleButtonView(iconName: showPortfolio ? "plus" : "info")
+                .animation(.none)
+                .background(
+                    CircleButtonAnimationView(animate: $showPortfolio)
+                )
+            Spacer()
+            Text(showPortfolio ? "Portfolio" : "Live Prices")
+                .font(.headline)
+                .fontWeight(.heavy)
+                .foregroundColor(Color.theme.accent)
+                .animation(.none)
+            Spacer()
+            CircleButtonView(iconName: "chevron.right")
+                .rotationEffect(Angle(degrees: showPortfolio ? 180 : 0))
+                .onTapGesture {
+                    withAnimation(.spring()) {
+                        self.showPortfolio.toggle()
+                    }
+                }
+            
+        }
+        .padding(.horizontal)
     }
 }
 
